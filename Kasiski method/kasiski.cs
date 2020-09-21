c6081188d1ac055ef6615b2eee186321c7390a83
Domchix <domusis937@gmail.com>
2020-09-21 13:58:27 +0300

    Kasiski method update

--- Kasiski method/kasiski.cs
+++ Kasiski method/kasiski.cs
@@ -13,31 +13,21 @@ namespace Cypher
 
     public static class Kasiski
     {
-        public static int MinKeyLength { get; set; } = 3;
-        public static int MaxKeyLength { get; set; } = 70;
+        public static int MinKeyLength { get; set; } = 4;
+        public static int MinDigramLength {get;set;} = 4;
+        public static int MaxDigramLength { get; set; } = 4;
         private static IEnumerable<int> GetDivisors(int n)
         {
             return from a in Enumerable.Range(2, n / 2)
                 where n % a == 0
                 select a;
         }
-        private static int GCD(int a, int b)
-        {
-            if (b == 0)
-            {
-                return a;
-            }
-            else
-            {
-                return GCD(b, a % b);
-            }
-        }
 
         public static int FindKeyLength(string text)
         {
             // Find all matching text parts
             List<Pair> matchingPairs = new List<Pair>();
-            foreach (var digramLength in Enumerable.Range(MinKeyLength, MaxKeyLength))
+            foreach (var digramLength in Enumerable.Range(MinDigramLength, MaxDigramLength))
             {
                 for (int i = 0; i < text.Length - digramLength; i++)
                 {
@@ -59,40 +49,28 @@ namespace Cypher
                     }
                 }
             }
-            // Analyze matching parts matrix for most common deviders
-            List<Pair> greatestCommonDeviders = new List<Pair>();
-            for(int i = 0; i < matchingPairs.Count(); i++)
-                for(int j = i + 1; j < matchingPairs.Count(); j++)
-                {
-                    var keyLength = GCD(matchingPairs[i].PeriodLength, matchingPairs[j].PeriodLength);
-                    if (greatestCommonDeviders.Any(n => n.PeriodLength == keyLength))
-                        greatestCommonDeviders.FirstOrDefault(n => n.PeriodLength == keyLength).CountOfSubstrings++;
-                    else
-                        greatestCommonDeviders.Add(new Pair
-                        {
-                            PeriodLength = keyLength,
-                            CountOfSubstrings = 1
-                        });
-                }
 
-            List<Pair> gcd = new List<Pair>();
+            // Get all different deviders and count ammount
+            List<Pair> deviders = new List<Pair>();
             foreach(Pair matchingPair in matchingPairs){
                 foreach(var divisor in GetDivisors(matchingPair.PeriodLength).Append(matchingPair.PeriodLength)){
-                    if (gcd.Any(n => n.PeriodLength == divisor))
-                        gcd.FirstOrDefault(n => n.PeriodLength == divisor).CountOfSubstrings++;
+                    if (deviders.Any(n => n.PeriodLength == divisor))
+                        deviders.FirstOrDefault(n => n.PeriodLength == divisor).CountOfSubstrings += matchingPair.CountOfSubstrings;
                     else
-                        gcd.Add(new Pair
+                        deviders.Add(new Pair
                         {
                             PeriodLength = divisor,
-                            CountOfSubstrings = 1
+                            CountOfSubstrings = matchingPair.CountOfSubstrings
                         });
                 }
             }
             // Filter out smaller keys, order by count and take the highest one
-            var a = greatestCommonDeviders.OrderByDescending(n => n.CountOfSubstrings).ToList();
-            var b = gcd.OrderByDescending(n => n.CountOfSubstrings).ToList();
-            Console.WriteLine(b[0].PeriodLength);
-            return greatestCommonDeviders.Where(n => n.PeriodLength > MinKeyLength).OrderByDescending(n => n.CountOfSubstrings).ToList()[0].PeriodLength;
+            var matrix = deviders.OrderByDescending(n => n.CountOfSubstrings).ToList();
+            Console.WriteLine("Devidor  | Number of pairs");
+            foreach(var item in matrix.Take(10)){
+                Console.WriteLine($"{item.PeriodLength} \t | {item.CountOfSubstrings}");
+            }
+            return deviders.Where(n => n.PeriodLength >= MinKeyLength).OrderByDescending(n => n.CountOfSubstrings).ToList()[0].PeriodLength;
         }
     }
 }
